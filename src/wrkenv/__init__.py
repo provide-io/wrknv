0a412423ebe6cc5a4d26c4def09a37391d767fb0
Tim Perkins <tim@livingstaccato.com>
2025-08-09 14:33:08 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/wrkenv/__init__.py
+++ src/wrkenv/__init__.py
@@ -10,11 +10,15 @@ Can be used standalone or integrated with other tools like TofuSoup.
 
 __version__ = "0.1.0"
 
+# Import workenv module to make it accessible
+from . import workenv
+
 # Public API
 from .workenv.config import WorkenvConfig, WorkenvConfigError
 from .workenv.managers.factory import get_tool_manager, get_supported_tools
 
 __all__ = [
+    "workenv",
     "WorkenvConfig",
     "WorkenvConfigError", 
     "get_tool_manager",
