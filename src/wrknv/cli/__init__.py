67f7ff10daaa9defd0f5137b2603abd6801f629d
Tim <code@tim.life>
2025-08-31 21:41:30 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/wrknv/cli/__init__.py
+++ src/wrknv/cli/__init__.py
@@ -6,12 +6,8 @@ Command-line interface for wrknv tool management.
 
 from wrknv.cli.hub_cli import create_cli, main
 
-# Legacy compatibility - will be removed
-try:
-    from wrknv.cli.main import entry_point, workenv_cli
-except ImportError:
-    # If old modules don't exist, use new ones
-    entry_point = main
-    workenv_cli = create_cli
+# Compatibility aliases
+entry_point = main
+workenv_cli = create_cli
 
 __all__ = ["create_cli", "main", "workenv_cli", "entry_point"]
