a9df05960f7bfc3fc66e35494bd901b608bd6756
Tim <code@tim.life>
2026-01-10 09:50:26 -0800

    🔼⚙️ [skip ci] auto-commit

--- src/wrknv/cli/commands/check.py
+++ src/wrknv/cli/commands/check.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2025 provide.io llc. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) provide.io llc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -41,7 +41,7 @@ def check_group() -> None:
 HEADER_SHEBANG = "#!/usr/bin/env python3"
 HEADER_LIBRARY = "# "
 SPDX_BLOCK = [
-    "# SPDX-FileCopyrightText: Copyright (c) 2025 provide.io llc. All rights reserved.",
+    "# SPDX-FileCopyrightText: Copyright (c) provide.io llc. All rights reserved.",
     "# SPDX-License-Identifier: Apache-2.0",
     "#",
 ]
