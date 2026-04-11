eb17974efb013afffa44cc5996ac054cfba786e8
Tim <code@tim.life>
2026-04-11 13:13:09 -0700

    extract go mirror URL to defaults.py constant

--- src/wrknv/managers/go.py
+++ src/wrknv/managers/go.py
@@ -16,7 +16,7 @@ import re
 from provide.foundation import logger
 from provide.foundation.transport import get
 
-from wrknv.config.defaults import GO_DOWNLOAD_API_URL
+from wrknv.config.defaults import GO_DOWNLOAD_API_URL, GO_MIRROR_DEFAULT
 
 from .base import BaseToolManager, ToolManagerError
 
@@ -70,7 +70,7 @@ class GoManager(BaseToolManager):
         arch = platform_info["arch"]
 
         # Use custom mirror if configured
-        mirror_url = self.config.get_setting("go_mirror", "https://go.dev/dl")
+        mirror_url = self.config.get_setting("go_mirror", GO_MIRROR_DEFAULT)
 
         return f"{mirror_url.rstrip('/')}/go{version}.{os_name}-{arch}.tar.gz"
 
